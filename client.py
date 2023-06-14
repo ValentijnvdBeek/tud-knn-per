a255be10a2fc1e285be01850b2b704989ea2bc4a
julioIns22 <julio@insocial.nl>
2023-06-14 12:15:27 +0200

    Revert "set capacity to 1"
    This reverts commit b21425414615c5408287f32e35beba1db30da921.

--- client.py
+++ client.py
@@ -598,8 +598,7 @@ class KNNPerClient(Client):
         train_loss, train_acc = self.learner.evaluate_iterator(self.val_iterator)
         test_loss, test_acc = self.learner.evaluate_iterator(self.test_iterator)
 
-        self.capacity = 1
-
+        self.capacity = capacity
         self.clear_datastore()
         self.build_datastore()
         self.gather_knn_outputs()
